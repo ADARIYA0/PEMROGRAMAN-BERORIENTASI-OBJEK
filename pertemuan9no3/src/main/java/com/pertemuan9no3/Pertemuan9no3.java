148f040fbc06f99c8056381c9a890445c2e1902e
Adrian Anugerah Maulana <137357459+ADARIYA0@users.noreply.github.com>
2024-08-16 14:53:15 +0700

    Add files via upload

---
+++ pertemuan9no3/src/main/java/com/pertemuan9no3/Pertemuan9no3.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.pertemuan9no3;
+
+/**
+ *
+ * @author anugerahworkplus
+ */
+public class Pertemuan9no3 {
+
+    public static void main(String[] args) {
+        int i =1;
+        while (i < 6) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
