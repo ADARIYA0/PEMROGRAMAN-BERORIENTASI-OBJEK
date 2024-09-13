bc467ad5a4a0362dc2c250a1385dd71d92494643
Adrian Anugerah Maulana <137357459+ADARIYA0@users.noreply.github.com>
2024-09-13 09:57:22 +0700

    Add files via upload

---
+++ Pertemuan16-AccessModifier/PublicPrivateModifier/src/main/java/com/pertemuan16/Pertemuan16Sub.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.pertemuan16;
+
+class Pertemuan16Sub extends Pertemuan16 {
+    // Implementing the abstract methods
+    @Override
+    protected double penjumlahan() {
+        return A + B;
+    }
+    
+    @Override
+    protected double pengurangan() {
+        return A - B;
+    }
+    
+    @Override
+    protected double perkalian() {
+        return A * B;
+    }
+    
+    @Override
+    protected double pembagian() {
+        return A / B;
+    }
+}
